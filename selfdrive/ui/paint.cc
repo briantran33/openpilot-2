8ca3ab8aad77d5c9fef7148bf9c358b35694a615
sunnyhaibin <haibin.wen3@gmail.com>
2021-08-24 03:11:43 -0400

    Feature: Dynamic Lane Profile Change (DLPC)

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -285,8 +285,8 @@ static void draw_laneless_button(UIState *s) {
   if (s->vipc_client->connected) {
     int btn_w = 140;
     int btn_h = 140;
-    int btn_x1 = s->fb_w - btn_w - 195;
-    int btn_y = 1080 - btn_h - 35;
+    int btn_x1 = s->fb_w - btn_w - 195 -20;
+    int btn_y = 1080 - btn_h - 35 - (btn_h / 2) - 10;
     int btn_xc1 = btn_x1 + (btn_w/2);
     int btn_yc = btn_y + (btn_h/2);
     nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
